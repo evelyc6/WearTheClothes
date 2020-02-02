9a388effd368572aefe738dc240a0fcbeeb7dfde
Evelyn git config --global user.name Evelyn <panteat@uci.edu>
2020-02-02 05:30:53 -0800

    Updates

---
+++ precache-manifest.820f4af0ce07edac9076523180e7d5da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c410d255308b19427ea2f890068df735",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e4a86492db90a3ce87b",
+    "url": "/static/css/main.ece3459b.chunk.css"
+  },
+  {
+    "revision": "854e3e73683c12f4b4d6",
+    "url": "/static/js/2.c71a9145.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.c71a9145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4a86492db90a3ce87b",
+    "url": "/static/js/main.15b8be1a.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "ad238b7bd5a37fbc3622b8dc0042879b",
+    "url": "/static/media/background.ad238b7b.jpg"
+  }
+]);
