7a8e14036a0dc30cdfea1adf884eacf37999feb9
Evelyn git config --global user.name Evelyn <panteat@uci.edu>
2020-02-22 14:38:39 -0800

    doing it ourselves

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Home from './Home';
-
+import Team from './Team';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route strict exact path="/" component={Home}/>
+      {/* </Switch>
+      <Switch> */}
+        <Route strict exact path="/meet-the-team" component = {Team}/>
       </Switch>
     </Router>
   );
---
+++ src/Team.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function Team() {
+  return (
+    <div className="background">
+      <div>
+        <div className = "topBanner">
+            <header className = "Top">WearTHE Clothes</header>
+            <header className = "appDesc">Meet the Team.</header>
+    `   </div>
+      </div>
+    </div>
+  );
+}
+
+export default Team;
